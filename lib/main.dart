37cf0649e3858c8ea6674de45fb763c055bf495a
Aspharier <codeworkashish@gmail.com>
2024-05-20 15:10:46 +0530

    final commit added TextButton - that changes the images of the dice added random function that generate random number between 1-6. that's it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black45,
         appBar: AppBar(
-          title: const Center(child: Text('Dicee')),
-          backgroundColor: Colors.red,
+          title: const Center(
+            child: Text(
+              'Dicee',
+              style: TextStyle(
+                fontSize: 30,
+                letterSpacing: 3,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.black,
         ),
         body: DicePage(),
       ),
@@ -16,9 +28,57 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // int leftDiceNumber = 1;
+  // int rightDiceNumber = 1;
+  int diceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  diceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('i2/dice$diceNumber.png'),
+            ),
+          ),
+          /*Expanded(
+            flex: 1,
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1; // 0+1=1,5+1=6
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),*/
+          /*Expanded(
+            flex: 1,
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),*/
+        ],
+      ),
+    );
   }
 }
